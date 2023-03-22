aee3d42ae930d899968b323e7bb9457b5a072fbd
kgbcupcake <kgbcupcake@gmail.com>
2023-03-22 18:44:54 -0400

    Update MainEncounter.cs

--- Entities/Encounters/MainEncounter.cs
+++ Entities/Encounters/MainEncounter.cs
@@ -9,7 +9,7 @@ namespace DungeonCrawlers.Entities.Encounters
 	{
 		static readonly Random rand = new Random();
 		public static Player currentPlayer = new Player();
-		//private readonly IAdventureService adventureServices;
+		
 	
 		
 
